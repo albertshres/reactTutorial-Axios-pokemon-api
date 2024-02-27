a8e663674945862ee6ab076314652c126b20d454
Albert <albertshres2059@gmail.com>
2024-02-27 14:59:29 +0545

    used pokemon api

--- src/ComA.jsx
+++ src/ComA.jsx
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const ComA = () => {
   const [num, setNum] = useState();
+  const [Name, setName] = useState();
+  const [moves, setMove] = useState();
+  useEffect(() => {
+    //alert("hi");
+    // this is a asynchronous function , and await is resposible to wait for the getting data
+    async function getData() {
+      const res = await axios.get(` https://pokeapi.co/api/v2/pokemon/${num}`);
+
+      console.log(res);
+      setName(res.data.name);
+      setMove(res.data.moves.length);
+    }
+    getData();
+  });
   return (
     <>
-      <h1>You have choose {num} value</h1>
+      <h1>
+        You have choose <span style={{ color: "red" }}>{num} value</span>
+      </h1>
+      <h1>
+        My name is <span style={{ color: "red" }}> {Name}</span>
+      </h1>
+      <h1>
+        I have <span style={{ color: "red" }}> {moves} moves</span>
+      </h1>
       <select
         value={num}
         onChange={(event) => {

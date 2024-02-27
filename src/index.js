7c471339fd886aa6fd4e674f0f42754efd91e15c
Albert <albertshres2059@gmail.com>
2024-02-27 14:05:56 +0545

    added new component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import App from "./App";
+import ComA from "./ComA";
 
-ReactDOM.render( <App/>, document.getElementById("root"));
+ReactDOM.render(<ComA />, document.getElementById("root"));
